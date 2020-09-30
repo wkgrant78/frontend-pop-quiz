9c3426405ec761205b05ba1af25741582985997f
Wendy Grant <wendykgrant.tx@gmail.com>
2020-09-29 21:57:41 -0500

    Added comments

--- scores.js
+++ scores.js
@@ -1,15 +1,15 @@
 var restartBtn = document.querySelector("button.restartBtn"),
     clearBtn = document.querySelector("button.clearBtn"),
-    // get the highScores list and turn it back into an object
+    // JSON.parse converts data from web server to JS object
     highScores = JSON.parse(localStorage.getItem("highScores") || "[]"),
     scoreList = document.getElementById("score-list");
 
-    // sort scores from high to low
+    // sort scores 
     highScores.sort(function (a,b){
         return b.score - a.score
     })
 
-    // display the scores
+    // display rank/userInitials/score
     for (var s = 0; s < highScores.length; s++) {
         var newLi = document.createElement("li")
         newLi.textContent = highScores[s].name + " - " + highScores[s].score
@@ -17,7 +17,7 @@ var restartBtn = document.querySelector("button.restartBtn"),
     }
 
 
-// click handlers for restart and clearing scoreboard
+// click event - 'try again' / 'clear score'
 clearBtn.addEventListener("click", function () {
     localStorage.clear();
     history.back()
